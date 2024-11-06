9e9c9faf077c2ce6803ae2c4fcc8e18bf4432212
MariaGargoles <mariagargoles@live.com>
2024-11-06 10:17:06 +0100

    add cypress6

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: "http://appreact.s3-website.eu-west-3.amazonaws.com/",
-    specPattern: "cypress/e2e/**/*.spec.{js,jsx,ts,tsx}",
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
   },
 });
--- cypress/e2e/loading.cy.js
+++ cypress/e2e/loading.cy.js
@@ -1,7 +1,10 @@
 describe("Verificar que la página carga correctamente", () => {
   it("Debería cargar la página principal sin errores", () => {
     cy.visit("/");
-
+    cy.url().should(
+      "eq",
+      "http://appreact.s3-website.eu-west-3.amazonaws.com/"
+    );
     cy.get("body").should("be.visible");
   });
 });
