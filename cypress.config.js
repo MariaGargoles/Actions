5905b14c818d6f50bcb6db84c91665ba67145bb0
MariaGargoles <mariagargoles@live.com>
2024-11-06 09:53:33 +0100

    add cypress4

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,8 @@
-// cypress.config.js
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "nv41qi",
   e2e: {
-    baseUrl: "http://appreact.s3-website.eu-west-3.amazonaws.com/",
-    specPattern: "cypress/e2e/**/*.spec.{js,jsx,ts,tsx}",
+    setupNodeEvents(on, config) {},
   },
 });
---
+++ cypress/e2e/loading.cy.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    baseUrl: "http://appreact.s3-website.eu-west-3.amazonaws.com/",
+    specPattern: "cypress/e2e/**/*.spec.{js,jsx,ts,tsx}",
+  },
+});
